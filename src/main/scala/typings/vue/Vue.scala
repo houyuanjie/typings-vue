91ede7b1214c32f774b156d198fcaa679d04c129
houyuanjie <houyuanjie555@qq.com>
2023-03-10 12:57:17 +0800

    vue api import

--- src/main/scala/typings/vue/Vue.scala
+++ src/main/scala/typings/vue/Vue.scala
@@ -14,11 +14,11 @@ final val VUE = "vue"
 object Vue extends js.Object with AppConstructor with General with Reactivity with Lifecycle with Injection:
 
   // AppConstructor
-  def createApp(rootComponent: js.Object, rootProps: js.UndefOr[js.Object] = js.undefined): VueApp = js.native
-  def createSSRApp(rootComponent: js.Object, rootProps: js.UndefOr[js.Object] = js.undefined): VueApp = js.native
+  def createApp(rootComponent: js.Object, rootProps: js.UndefOr[js.Object] = js.undefined): App = js.native
+  def createSSRApp(rootComponent: js.Object, rootProps: js.UndefOr[js.Object] = js.undefined): App = js.native
 
   // General
-  val version: String = js.native
+  def version: String = js.native
   def nextTick(callback: js.UndefOr[js.Function0[Unit]] = js.undefined): js.Promise[Unit] = js.native
   def defineComponent(component: js.Object): js.Dynamic = js.native
   def defineAsyncComponent(source: js.Object): js.Dynamic = js.native
@@ -26,18 +26,17 @@ object Vue extends js.Object with AppConstructor with General with Reactivity wi
 
   // ReactivityCore
   def ref(value: js.Any): js.Dynamic = js.native
-  def computed(options: js.Object): js.Dynamic = js.native
+  def computed(options: js.Object, debuggerOptions: js.UndefOr[js.Object] = js.undefined): js.Dynamic = js.native
   def reactive(target: js.Object): js.Dynamic = js.native
   def readonly(target: js.Object): js.Dynamic = js.native
   def watchEffect(effect: js.Object, options: js.UndefOr[js.Object] = js.undefined): js.Dynamic = js.native
   def watchPostEffect(effect: js.Object, options: js.UndefOr[js.Object] = js.undefined): js.Dynamic = js.native
   def watchSyncEffect(effect: js.Object, options: js.UndefOr[js.Object] = js.undefined): js.Dynamic = js.native
-  def watch(source: js.Object, callback: js.Object, options: js.UndefOr[js.Object] = js.undefined): js.Dynamic =
-    js.native
+  def watch(source: js.Object, callback: js.Object, options: js.UndefOr[js.Object] = js.undefined): js.Dynamic = js.native
 
   // ReactivityUtilities
-  def isRef(r: js.Object): js.Dynamic = js.native
-  def unref(ref: js.Object): js.Dynamic = js.native
+  def isRef(r: js.Any): js.Dynamic = js.native
+  def unref(ref: js.Any): js.Dynamic = js.native
   def toRef(`object`: js.Object, key: js.Any, defaultValue: js.UndefOr[js.Any] = js.undefined): js.Dynamic = js.native
   def toRefs(`object`: js.Object): js.Dynamic = js.native
   def isProxy(value: js.Any): Boolean = js.native
@@ -52,7 +51,7 @@ object Vue extends js.Object with AppConstructor with General with Reactivity wi
   def shallowReadonly(target: js.Object): js.Dynamic = js.native
   def toRaw(proxy: js.Any): js.Dynamic = js.native
   def markRaw(value: js.Object): js.Dynamic = js.native
-  def effectScope(detached: js.UndefOr[Boolean]): js.Dynamic = js.native
+  def effectScope(detached: js.UndefOr[Boolean] = js.undefined): js.Dynamic = js.native
   def getCurrentScope(): js.UndefOr[js.Dynamic] = js.native
   def onScopeDispose(fn: js.Function0[Unit]): Unit = js.native
 
@@ -63,11 +62,13 @@ object Vue extends js.Object with AppConstructor with General with Reactivity wi
   def onBeforeMount(callback: js.Function0[Unit]): Unit = js.native
   def onBeforeUpdate(callback: js.Function0[Unit]): Unit = js.native
   def onBeforeUnmount(callback: js.Function0[Unit]): Unit = js.native
-  def onErrorCaptured(callback: js.Function3[js.Any, js.Any, String, Boolean | Unit]): Unit = js.native
+  def onErrorCaptured(callback: js.Function3[js.Any, js.Object | Null, String, Boolean | Unit]): Unit = js.native
   def onActivated(callback: js.Function0[Unit]): Unit = js.native
   def onDeactivated(callback: js.Function0[Unit]): Unit = js.native
+  def onServerPrefetch(callback: js.Function0[js.Promise[js.Any]]): Unit = js.native
 
   // Injection
   def provide(key: String, value: js.Any): Unit = js.native
   def inject(key: String): js.UndefOr[js.Dynamic] = js.native
   def inject(key: String, defaultValue: js.Any): js.Dynamic = js.native
+  def inject(key: String, defaultValue: js.Function0[js.Any], treatDefaultAsFactory: Boolean): js.Dynamic = js.native
--- src/main/scala/typings/vue/VueApp.scala
+++ src/main/scala/typings/vue/VueApp.scala
@@ -15,12 +15,12 @@ class VueApp extends js.Object with App:
   self =>
   def mount(rootContainer: Element | String): js.Dynamic = js.native
   def unmount(): Unit = js.native
-  def provide(key: js.Symbol | String, value: js.Object): self.type = js.native
+  def provide(key: js.Symbol | String, value: js.Any): self.type = js.native
   def component(name: String): js.UndefOr[js.Dynamic] = js.native
   def component(name: String, component: js.Object): self.type = js.native
   def directive(name: String): js.UndefOr[js.Dynamic] = js.native
   def directive(name: String, directive: js.Object): self.type = js.native
   def use(plugin: js.Object, options: js.UndefOr[js.Array[js.Any]] = js.undefined): self.type = js.native
   def mixin(mixin: js.Object): self.type = js.native
-  val version: String = js.native
-  var config: js.Dynamic = js.native
+  def version: String = js.native
+  def config: js.Dynamic = js.native
